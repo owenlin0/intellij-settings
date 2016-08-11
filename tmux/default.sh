ce8d514d3050da6fe277befd175afe86f7ec4c92
Owen Lin <owen.lin@airbnb.com>
2016-08-10 22:18:28 -0700

    update dotfiles and tmux scripts

--- tmux/default.sh
+++ tmux/default.sh
@@ -1,30 +1,39 @@
-tmux new-session -s default -d
+SESSION_NAME=default
+
+tmux has-session -t $SESSION_NAME 2> /dev/null
+if [ $? -eq 0 ]
+then
+  tmux attach -t $SESSION_NAME
+  exit
+fi
+
+tmux new-session -s $SESSION_NAME -d
 
 # window: monorail
 tmux rename-window monorail
-tmux send-keys -t default 'cd ~/repos/airbnb' C-m
+tmux send-keys -t $SESSION_NAME 'cd ~/repos/airbnb' C-m
 
 # window
 tmux new-window
-tmux send-keys -t default 'cd ~/repos/airbnb' C-m
+tmux send-keys -t $SESSION_NAME 'cd ~/repos/airbnb' C-m
 
 # window: rookery
 tmux new-window -n rookery
-tmux send-keys -t default 'cd ~/repos/rookery' C-m
+tmux send-keys -t $SESSION_NAME 'cd ~/repos/rookery' C-m
 
 # window
 tmux new-window
-tmux send-keys -t default 'cd ~/repos/rookery' C-m
+tmux send-keys -t $SESSION_NAME 'cd ~/repos/rookery' C-m
 
 # window: data
 tmux new-window -n data
-tmux send-keys -t default 'cd ~/repos/data' C-m
+tmux send-keys -t $SESSION_NAME 'cd ~/repos/data' C-m
 
 # window
 tmux new-window
-tmux send-keys -t default 'cd ~/repos/data' C-m
+tmux send-keys -t $SESSION_NAME 'cd ~/repos/data' C-m
 
 ### Select 1st window
-tmux next-window -t default
+tmux next-window -t $SESSION_NAME
 
-tmux attach -t default
+tmux attach -t $SESSION_NAME
--- tmux/vagrant.sh
+++ tmux/vagrant.sh
@@ -1,36 +1,45 @@
-tmux new-session -s vagrant -d
+SESSION_NAME=vagrant
+
+tmux has-session -t $SESSION_NAME 2> /dev/null
+if [ $? -eq 0 ]
+then
+  tmux attach -t $SESSION_NAME
+  exit
+fi
+
+tmux new-session -s $SESSION_NAME -d
 
 ### Monorail
 # first pane
 tmux rename-window monorail
-tmux send-keys -t vagrant 'cd ~/repos/airbnb && vagrant ssh' C-m
-tmux send-keys -t vagrant 'cd repos/airbnb && bundle install && zeus start' C-m
+tmux send-keys -t $SESSION_NAME 'cd ~/repos/airbnb && vagrant ssh' C-m
+tmux send-keys -t $SESSION_NAME 'cd repos/airbnb && bundle install && zeus start' C-m
 
 # second pane
-tmux split-window -h -t vagrant
-tmux send-keys -t vagrant 'sleep $[ ( $RANDOM % 5 ) + 1 ]s; cd ~/repos/airbnb && vagrant ssh' C-m
-tmux send-keys -t vagrant 'cd repos/airbnb' C-m
+tmux split-window -h -t $SESSION_NAME
+tmux send-keys -t $SESSION_NAME 'sleep $[ ( $RANDOM % 5 ) + 1 ]s; cd ~/repos/airbnb && vagrant ssh' C-m
+tmux send-keys -t $SESSION_NAME 'cd repos/airbnb' C-m
 
 # third pane
 tmux select-pane -L
-tmux split-window -v -t vagrant
-tmux send-keys -t vagrant 'sleep $[ ( $RANDOM % 5 ) + 1 ]s; cd ~/repos/airbnb && vagrant ssh' C-m
-tmux send-keys -t vagrant 'cd repos/airbnb' C-m
+tmux split-window -v -t $SESSION_NAME
+tmux send-keys -t $SESSION_NAME 'sleep $[ ( $RANDOM % 5 ) + 1 ]s; cd ~/repos/airbnb && vagrant ssh' C-m
+tmux send-keys -t $SESSION_NAME 'cd repos/airbnb' C-m
 
 ### Rookery
 tmux new-window -n rookery
 # first pane
-tmux send-keys -t vagrant 'sleep $[ ( $RANDOM % 5 ) + 1 ]s; cd ~/repos/airbnb && vagrant ssh' C-m
-tmux send-keys -t vagrant 'cd repos/rookery' C-m
+tmux send-keys -t $SESSION_NAME 'sleep $[ ( $RANDOM % 5 ) + 1 ]s; cd ~/repos/airbnb && vagrant ssh' C-m
+tmux send-keys -t $SESSION_NAME 'cd repos/rookery' C-m
 # second pane
-tmux split-window -h -t vagrant
-tmux send-keys -t vagrant 'sleep $[ ( $RANDOM % 5 ) + 1 ]s; cd ~/repos/airbnb && vagrant ssh' C-m
-tmux send-keys -t vagrant 'cd repos/rookery' C-m
+tmux split-window -h -t $SESSION_NAME
+tmux send-keys -t $SESSION_NAME 'sleep $[ ( $RANDOM % 5 ) + 1 ]s; cd ~/repos/airbnb && vagrant ssh' C-m
+tmux send-keys -t $SESSION_NAME 'cd repos/rookery' C-m
 
 ### Open window
 tmux new-window
 
 ### Select 1st window
-tmux next-window -t vagrant
+tmux next-window -t $SESSION_NAME
 
-tmux attach -t vagrant
+tmux attach -t $SESSION_NAME
